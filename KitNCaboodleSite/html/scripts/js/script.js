13e35d433b944a82c103c4550cfb4f837b892aba
Jamison <jamiedude224@gmail.com>
2021-11-02 13:28:01 -0700

    Add project files.

---
+++ KitNCaboodleSite/html/scripts/js/script.js
@@ -0,0 +1,20 @@
+﻿/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("navBar").style.display = "flex";
+}
+
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeNav() {
+    document.getElementById("navBar").style.display = "none";
+}
+
+$(window).resize(function () {
+    if ($(this).width() >= 1080) {
+        openNav();
+    }
+});
+$(window).resize(function () {
+    if ($(this).width() < 1080) {
+        closeNav();
+    }
+});
